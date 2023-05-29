79ab85c1c4660800899b9f3be648bb57d3ba4ad7
DevShaded <devshaded@devshaded.com>
2023-05-29 13:28:24 +0200

    Added controllers + routes and services

--- app/Http/Services/User/UserService.php
+++ app/Http/Services/User/UserService.php
@@ -3,7 +3,9 @@
 namespace App\Http\Services\User;
 
 use App\Http\Resources\User\UserResource;
+use App\Models\User\Country\Country;
 use App\Models\User\User;
+use Illuminate\Support\Facades\Storage;
 
 class UserService
 {
@@ -17,6 +19,41 @@ class UserService
 
     public function getUser(string $id, $collection = false): UserResource
     {
-        return new UserResource(User::with(['posts'])->findOrFail($id));
+        return new UserResource(User::with(['information', 'information.country'])->findOrFail($id));
+    }
+
+    public function getUserByName(string $name, $collection = false): UserResource
+    {
+        return new UserResource(User::with(['information', 'information.userCountry', 'followers'])->where('name', $name)->firstOrFail());
+    }
+
+    public function getCountryByName(string $name)
+    {
+        $country = Country::where('name', $name)->firstOrFail();
+
+        return $country->id;
+    }
+
+    public function storeAvatar(array|\Illuminate\Http\UploadedFile|null $avatar, UserResource $user): bool
+    {
+        $defaultAvatarPattern = '/^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/i';
+
+        if (! preg_match($defaultAvatarPattern, $user->avatar) && ! Storage::exists($user->avatar)) {
+            Storage::delete('public/avatars/'.$user->avatar);
+        }
+
+        $extension = $avatar->getClientOriginalExtension();
+        $filename = 'avatar-'.$user->id.'.'.$extension;
+
+        $avatar->storeAs('public/avatars', $filename);
+
+        return $user->update([
+            'avatar' => $filename,
+        ]);
+    }
+
+    public function getCountries(): \Illuminate\Database\Eloquent\Collection
+    {
+        return Country::all();
     }
 }
